c56cf62c9b6af1ac2041c333af919286554184cc
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-07-24 21:08:03 +0300

    Add add, remove, update task | id is undefined

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,46 @@
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+
 const App = () => {
+  const [socket, setSocket] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState('');
+
+  useEffect(() => {
+    const socket = io('http://localhost:8000');
+    setSocket(socket);
+
+    socket.on('removeTask', (taskId) => {
+      removeTask(taskId);
+    });
+    socket.on('addTask', (task) => {
+      addTask(task);
+    });
+    socket.on('updateTask', (tasks) => {
+      updateTasks(tasks);
+    });
+  });
+
+  const removeTask = (taskId, isLocal) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== taskId));
+    if (isLocal) {
+      socket.emit('removeTask', taskId);
+    }
+  };
+
+  const addTask = (task) => {
+    setTasks((tasks) => [...tasks, task]);
+  };
+
+  const updateTasks = (tasksData) => {
+    setTasks(tasksData);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    addTask({ name: taskName });
+  };
+
   return (
     <div className="App">
       <header>
@@ -9,21 +51,28 @@ const App = () => {
         <h2>Tasks</h2>
 
         <ul className="task-section_list" id="task-list">
-          <li className="task">
-            Shopping<button className="btn btn-red">Remove</button>
-          </li>
-          <li className="task">
-            Go out with a dog<button className="btn btn-red">Remove</button>
-          </li>
+          {tasks.map((task) => (
+            <li className="task" key={task.id}>
+              {task.name}
+              <button
+                className="btn btn-red"
+                onClick={() => removeTask(task.id, true)}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
         </ul>
 
-        <form id="add-task-form">
+        <form id="add-task-form" onSubmit={(e) => submitForm(e)}>
           <input
             className="text-input"
             autoComplete="off"
             type="text"
             placeholder="Type your description"
             id="task-name"
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
           />
           <button className="btn" type="submit">
             Add button
